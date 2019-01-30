f3b8f21d49f722c8da01056b7bbf2be10c3381a4
Connor Radeloff <ConnorRadeloff1@gmail.com>
2019-01-30 16:55:34 -0700

    Auto-refresh of AuthTokens implemented

--- BUZZ/Core/CharacterManagement/CharacterManager.cs
+++ BUZZ/Core/CharacterManagement/CharacterManager.cs
@@ -4,10 +4,12 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 using BUZZ.Core.Verification;
 using BUZZ.Data;
 using BUZZ.Data.Models;
 using Polenter.Serialization;
+using Exception = System.Exception;
 
 namespace BUZZ.Core.CharacterManagement
 {
@@ -32,16 +34,17 @@ namespace BUZZ.Core.CharacterManagement
 
         public BindingList<BuzzCharacter> CharacterList { get; set; } = new BindingList<BuzzCharacter>();
 
+        public DispatcherTimer RefreshTimer = new DispatcherTimer()
+        {
+            Interval = TimeSpan.FromMinutes(10),
+            IsEnabled = false
+        };
+
         #endregion
 
         private CharacterManager()
         {
         }
-
-        private void RefreshAuthTokens()
-        {
-        }
-
 
         #region Public Methods
 
@@ -50,25 +53,53 @@ namespace BUZZ.Core.CharacterManagement
             currentInstance = new CharacterManager();
             DeserializeCharacterData();
             RefreshAccessTokensAsync();
+            StartAutoRefresh();
+        }
+
+        private static void StartAutoRefresh()
+        {
+            CurrentInstance.RefreshTimer.Tick += RefreshTimer_Tick;
+            CurrentInstance.RefreshTimer.IsEnabled = true;
+        }
+
+        private static void RefreshTimer_Tick(object sender, EventArgs e)
+        {
+            RefreshAccessTokensAsync();
         }
 
         private static async void RefreshAccessTokensAsync()
         {
-            var tasks = new List<Task>();
-            foreach (var buzzCharacter in CharacterManager.currentInstance.CharacterList )
+            try
             {
-                tasks.Add(buzzCharacter.RefreshAuthToken());
+                var tasks = new List<Task>();
+                foreach (var buzzCharacter in CharacterManager.currentInstance.CharacterList)
+                {
+                    tasks.Add(buzzCharacter.RefreshAuthToken());
+                }
+                await Task.WhenAll(tasks);
             }
-            await Task.WhenAll(tasks);
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
 
         private const string CharacterDataFilename = "Characters.bin";
 
         public static void DeserializeCharacterData()
         {
-            SharpSerializer searializer = new SharpSerializer();
-            CurrentInstance.CharacterList =
-                (BindingList<BuzzCharacter>)searializer.Deserialize(CharacterDataFilename);
+            try
+            {
+                SharpSerializer searializer = new SharpSerializer();
+                CurrentInstance.CharacterList =
+                    (BindingList<BuzzCharacter>)searializer.Deserialize(CharacterDataFilename);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
 
         public static void SerializeCharacterData()
